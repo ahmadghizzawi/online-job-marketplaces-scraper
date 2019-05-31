925f3526e379a5e6ce7560f514003822aa7df641
sultan <sultan961@live.com>
2019-05-31 13:21:00 +0200

    Create helpers.py
    the file that delete duplicate url moved to src/taskrabbit

---
+++ src/taskrabbit/helpers.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse , parse_qs
+from urllib.request import urlopen
+import json
+
+url = []
+parse = []
+parsed = []
+unique = []
+result = []
+with open('final.json') as f:
+	urls = json.load(f)
+
+for entry in urls:
+	url = (entry['url'])
+	parse = (urlparse(str(url)))
+	parsed = parse_qs(parse.query)
+	if not parsed['task_template_id'] in unique:
+		unique.append(parsed['task_template_id'])
+		result.append(entry)
+with open('queries.json', 'w') as nf:
+	json.dump(result, nf)
+
