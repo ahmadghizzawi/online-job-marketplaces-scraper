bc8507a536e879926c156e31afbf26f4e86bf55e
Ahmad Ghizzawi <ahmadghizzawi@gmail.com>
2019-07-01 23:36:43 +0200

    Add threading support

--- run_crawler.py
+++ run_crawler.py
@@ -1,13 +1,8 @@
-import argparse
 import json
-import os
-import subprocess
-import time
 import urllib.request
-import concurrent.futures
-import sys
-import asyncio
-import threading
+from slugify import slugify
+from multiprocessing.dummy import Pool as ThreadPool
+import parmap
 
 from src.helper import (
     parse_arguments,
@@ -15,18 +10,44 @@ from src.helper import (
     get_url,
     get_crawler,
 )
-from src.mistertemp_crawler import MistertempCrawler
 from src.queries import get_queries
-from src.crawler import Query, RankingItem, RankingItemEncoder, QueryEncoder
-from datetime import datetime
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from slugify import slugify
-from urllib.parse import urlparse
+from src.crawler import Query, RankingItemEncoder, QueryEncoder
+
+
+def crawl_query(query: Query, index, args, results_path, pictures_path):
+    """
+    Crawl a single query.
+    :param query: query to be crawled.
+    :param args: commandline arguments. Expects args.platform and args.webdriver
+    :param results_path
+    :param pictures_path
+    :param index: the number of the query
+    :return: query if the query failed or None if it did not.
+    """
+    print("query " + str(index))
+
+    Crawler = get_crawler(args.platform)
+    crawler = Crawler(query, args.webdriver)
+    try:
+        list_result = crawler.crawl()
+        with open(
+                results_path + "/" + slugify(query.city + "-" + query.title) + ".json",
+                "w",
+        ) as fout:
+            json.dump(list_result, fout, cls=RankingItemEncoder)
+        for result in list_result:
+            urllib.request.urlretrieve(
+                result.picture_url, pictures_path + "/" + result.id + ".jpg"
+            )
+    except Exception as e:
+        print(e)
+        crawler.exit()
+        return query
+
+    return None
 
 
 def main():
-
     args = parse_arguments()
 
     if args.queriesfile is None:
@@ -35,37 +56,20 @@ def main():
     with open(args.queriesfile) as f:
         entries = json.load(f)
 
-    res, pic, folder_path = create_output_folders(args.output)
+    results_path, pictures_path, root_output_path = create_output_folders(args.output)
 
     list_query = [
         Query(get_url(args.platform, entry), entry["title"], entry["city"])
         for entry in entries
     ]
 
-    Crawler = get_crawler(args.platform)
-    failed = []
+    indexes = [i for i in range(len(list_query))]
+    results = parmap.starmap(
+        crawl_query, zip(list_query, indexes), args, results_path, pictures_path, pm_processes=args.threads)
 
-    counter = 1
-    for query in list_query:
-        print("query " + str(counter))
-        counter += 1
-        crawler = Crawler(query, args.webdriver)
-        try:
-            list_result = crawler.crawl()
-            with open(
-                res + "/" + slugify(query.city + "-" + query.title) + ".json",
-                "w",
-            ) as fout:
-                json.dump(list_result, fout, cls=RankingItemEncoder)
-            for result in list_result:
-                urllib.request.urlretrieve(
-                    result.picture_url, pic + "/" + result.id + ".jpg"
-                )
-        except Exception as e:
-            failed.append(query)
-            print(e)
-            crawler.exit()
-    with open(folder_path + "/failed_queries.json", "w") as f:
+    failed = [query for query in results if query is not None]
+
+    with open(root_output_path + "/failed_queries.json", "w") as f:
         json.dump(failed, f, cls=QueryEncoder)
 
 
